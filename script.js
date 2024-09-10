60ecb7e3d2a89aa50045241ad91cc89be964df12
Mazhar Ahmed <ma9822649@gmail.com>
2024-09-10 14:07:11 -0700

    adding media quries

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
-// document.querySelector('#contact-form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     e.target.elements.name.value = '';
-//     e.target.elements.email.value = '';
-//     e.target.elements.message.value = '';
-//   });
 
   new WOW().init();
 
-  
 
-// MouseFollower.registerGSAP(gsap);
-  
-// const cursor = new MouseFollower({
-//   skewing: 3,
-//   stickDelta: 0.009,
-//   stateDetection:{
-//     '-pointer':'a,button',
-//     '-opaque': '[data-cursor-opaque]',
-//     '-active': '[data-cursor]'
-//   }
-// });
+// Function to send email using emailjs
+function sendEmail(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
 
-// const btn = document.querySelector('#btn');
-// btn.addEventListener('click', () => {
-//   btn.classList.toggle('active');
-// });
-
-
-function sendMail() {
   let params = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    message : document.getElementById("message").value,
-}
-emailjs.send("service_fr8tjsp","template_rv1s5nu",params).then(function(res){
-  alert("Email sent sucessfully..!")
-})
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs.send("service_fr8tjsp", "template_rv1s5nu", params)
+    .then(function(res) {
+      // Swal.fire("Email Sent Sucessfully..!");
+      Swal.fire({
+        title: "Email Sent Sucessfully..!",
+        text: "Thank you for Contacting us ",
+        icon: "sucess"
+      });
+      // Clear the form fields after successful submission
+      document.getElementById("contact-form").reset();
+    })
+    .catch(function(err) {
+      console.error("Failed to send email:", err);
+      alert("Failed to send email. Please try again later.");
+    });
 }
 
+// Attach the sendEmail function to the form's submit event
+document.getElementById("contact-form").addEventListener("submit", sendEmail);
+
+// $(document).ready(function() {
+//   $(window).on("load", function() {
+//     $(".loader-wrapper").fadeOut("100");
+//   });
+// });
+
+
+$(window).on("load", function() {
+  $(".loader-wrapper").fadeOut(200);
+});
+
+setTimeout(function() {
+  if ($(".loader-wrapper").is(":visible")) {
+    $(".loader-wrapper").fadeOut(200);  // Fallback in case the load event doesn't trigger
+  }
+}, 5000);  // Force fade out after 5 seconds
